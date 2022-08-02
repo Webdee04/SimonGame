22e95c8a529b04555d614ed3ef3f2f5cd31f9432
Webdee04 <107318988+Webdee04@users.noreply.github.com>
2022-08-02 23:26:28 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,91 @@
+var buttonColours = ["red" , "blue" , "green" , "yellow"];
+
+var gamePattern = [];
+
+var started = false;
+
+var level = 0;
+
+var userClickedPattern = [];
+
+$(document).keypress(function() {
+  if(!started){
+    $("h1").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+  if(userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if(userClickedPattern.length === gamePattern.length){
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+
+    }
+  }
+  else  {
+    console.log("failure");
+
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("h1").text("Game Over, Press any Key to Restart.");
+
+    startOver();
+  }
+
+}
+
+function nextSequence() {
+
+  userClickedPattern = [];
+  level++;
+
+  $("h1").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random()* 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("." + currentColor).addClass("pressed");
+  setTimeout(function(){
+    $("." + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
